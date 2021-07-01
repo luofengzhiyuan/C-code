cd8c55ecf9a570ec5b37ed471edd618bc529015a
SLFM <77730089+luofengzhiyuan@users.noreply.github.com>
2021-07-01 15:43:21 +0800

    Add files via upload

---
+++ 作业：2.求10个数的平均值.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int f1(void);
+int average(void);
+int main(void)
+{
+	printf("请输入十个数\n");
+	average();
+}
+int f1(void)
+{
+	int num[10],i,a;
+	int n = 0;
+	for(i = 0;i < 10;i++)
+	{
+		scanf("%d",&a);
+		num[i]=a;
+		n += num[i];
+	}
+	return n;
+}
+int average(void)
+{
+	printf("平均数为%lf\n",(double)f1()/10);
+}
