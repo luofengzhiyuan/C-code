5ea4f5e8f33bbf1b4f6e5482164b17528ae2a4c6
SLFM <77730089+luofengzhiyuan@users.noreply.github.com>
2021-07-01 15:41:21 +0800

    Add files via upload

---
+++ 1.向屏幕输出一行hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>			//标准输入输出头文件，提示编译器执行输入输出语句。
+int main(void)				//main函数，C程序的入口，有且只有一个。
+{
+	printf("hello\n");		//printf函数，print fuction 打印函数。
+	return 0;			//程序结束的地方，因为main函数定义为整形，且返回类型为空，所以返回值为0.
+}
---
+++ 2.两个数求和.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{
+	int num1,num2;
+	printf("请输入两个数字\n");
+	scanf("%d%d",&num1,&num2);
+	printf("%d\n",num1+num2);
+	return 0;
+}
