2fda6a13e75a281fef925f963caa98b1574fb61c
SLFM <77730089+luofengzhiyuan@users.noreply.github.com>
2021-07-03 20:59:10 +0800

    Add files via upload

---
+++ 作业：小学数学考试系统.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(void)
+{
+	int num1,num2;
+	printf("请输入两个数计算加减乘除：\n");
+	scanf("%d%d",&num1,&num2);
+	if(num1<=10&&num1>=0)
+	{
+		if(num2<=10&&num2>=0)
+		{
+			if(num1>=num2)
+			{
+			printf("%d加%d=%d\n",num1,num2,num1+num2);
+			printf("%d乘%d=%d\n",num1,num2,num1*num2);
+			printf("%d减%d=%d\n",num1,num2,num1-num2);
+			printf("%d除%d=%d余数为：%d\n",num1,num2,num1/num2,num1%num2);
+			}
+			if(num1<num2)
+			{
+			printf("%d加%d=%d\n",num1,num2,num1+num2);
+			printf("%d乘%d=%d\n",num1,num2,num1*num2);
+			printf("%d减%d小学阶段不涉及\n",num1,num2);
+			printf("%d除%d小学阶段不涉及\n",num1,num2);
+			}
+		}
+		else
+			printf("输入错误\n");
+	}
+		else
+		printf("输入错误\n");
+	return 0;
+}
